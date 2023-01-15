d637847fd4ffa9ecb170d23b88557e8f0b08922d
MaximilianLeyh <118059860+MaximilianLeyh@users.noreply.github.com>
2023-01-15 19:20:51 +0100

    animated clouds and walking character

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,6 +1,6 @@
 class Chicken extends MovableObject {
-    y = 380;
-    height = 60;
+    y = 360;
+    height = 80;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
